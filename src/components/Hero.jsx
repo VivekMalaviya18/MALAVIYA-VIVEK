f547052ccf0e9179b42aab97570de65fd4df1a74
VivekMalaviya18 <malaviyavivek56@gmail.com>
2025-02-20 14:09:26 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { LuImport } from 'react-icons/lu'
+import benson from '../assets/benson.webp'
+
+const Hero = () => {
+  return (
+    <section className='flex flex-col items-center justify-center'>
+        <h1 className='mt-16 overflow-hidden text-[12vw] font-semibold uppercase leading-none'>
+            Vivek <br /> Malaviya
+        </h1>
+        <div className="mt-8">
+            <a href="/MALAVIYA VIVEK RESUME KS.pdf"
+                target='_blank'
+                rel='noopener noreferrer'
+                download
+                className='flex items-center rounded-xl bg-lime-400 p-2 px-3 font-sans font-medium text-black hover:bg-lime-500'>
+                <span>Resume PDF</span>
+                <LuImport className='ml-2' />
+            </a>
+        </div>
+        <div className="w-full">
+            <img src={benson} alt='Vivek Malaviya' className='mt-8 h-96 w-full object-cover'/>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
