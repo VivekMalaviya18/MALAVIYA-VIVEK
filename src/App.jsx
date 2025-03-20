a234388e8d38891721d83a171ba40718be06117b
VivekMalaviya18 <malaviyavivek56@gmail.com>
2025-03-20 21:51:13 +0530

    minor change done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Marquee from './components/Marquee'
-import Projects from './components/Projects'
-import About from './components/About'
-import Work from './components/Work'
-import Contact from './components/Contact'
-import Education from './components/Education'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Marquee from "./components/Marquee";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Education from "./components/Education";
 
 const App = () => {
-  return (
-   <main className='font-light text-white antialiased selection:bg-lime-300 selection:text-black'>
-    <Navbar />
-    <Hero />
-    <Marquee />
-    <Projects />
-    <Education />
-    <About />
-    {/* <Work /> */}
-    <Contact />
-   </main>
-   
-  )
-}
+    return (
+        <main className="font-light text-white antialiased selection:bg-orange-300 selection:text-black">
+            <Navbar />
+            <Hero />
+            <Marquee />
+            <Projects />
+            <Education />
+            <About />
+            {/* <Work /> */}
+            <Contact />
+        </main>
+    );
+};
 
-export default App
+export default App;
